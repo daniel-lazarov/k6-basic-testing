d835dd4552485726d6514cf195e82d7222480134
daniel-lazarov <daniel.lazarov@gmail.com>
2024-12-02 17:42:53 +0200

    some non-functional tests with k6

---
+++ soak-test.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        {
+            duration: '20s',
+            target: 50,
+        },
+        {
+            duration: '5m',
+            target: 50,
+        },
+        {
+            duration: '20s',
+            target: 0,
+        }
+    ]
+}
+
+export default function () {
+    http.get('https://test.k6.io');
+    sleep(1);
+    http.get('https://test.k6.io/contacts.php');
+    sleep(2);
+    http.get('https://test.k6.io/news.php');
+    sleep(2);
+}
---
+++ spike-test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        {
+            duration: '1m',
+            target: 50,
+        },
+        {
+            duration: '20s',
+            target: 0,
+        },
+        {
+            duration: '30s',
+            target: 50,
+        },
+        {
+            duration: '20s',
+            target: 0,
+        }
+    ]
+}
+
+export default function () {
+    http.get('https://test.k6.io');
+    sleep(1);
+    http.get('https://test.k6.io/contacts.php');
+    sleep(2);
+    http.get('https://test.k6.io/news.php');
+    sleep(2);
+}
